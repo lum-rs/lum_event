e27941a26793abf95100db1064f3fc85695720dc
Torben Schweren <torben@schweren.dev>
2026-05-01 18:31:32 +0200

    add: EventRepeater transformations

--- src/event_repeater.rs
+++ src/event_repeater.rs
@@ -16,9 +16,9 @@ use crate::event::EventHandleError;
 
 use super::{Event, event::EventHandle};
 
-struct Attachment<T: Clone + Send> {
-    event: EventHandle<T>,
-    subscriber_id: u64,
+struct Attachment {
+    is_dropped: Box<dyn Fn() -> bool + Send + Sync + 'static>,
+    do_unsubscribe: Box<dyn Fn() + Send + Sync + 'static>,
 }
 
 #[derive(Debug, Error)]
@@ -55,24 +55,33 @@ pub enum ForwardingError {
     RepeaterDropped { event_repeater_name: String },
 }
 
-pub struct EventRepeater<T: Clone + Send + 'static> {
-    pub event: Event<T>,
+pub struct EventRepeater<IN: Clone + Send + 'static, OUT: Clone + Send + 'static = IN> {
+    pub event: Event<OUT>,
 
-    attachments: Arc<DashMap<u64, Attachment<T>>>,
+    attachments: Arc<DashMap<u64, Attachment>>,
     alive: Arc<()>,
-    event_queue_sender: UnboundedSender<T>,
+    event_queue_sender: UnboundedSender<IN>,
 }
 
-impl<T: Clone + Send + 'static> EventRepeater<T> {
+impl<T: Clone + Send + 'static> EventRepeater<T, T> {
     pub fn new(name: impl Into<String>) -> Self {
+        Self::new_with_transform(name, |data| data)
+    }
+}
+
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> EventRepeater<IN, OUT> {
+    pub fn new_with_transform(
+        name: impl Into<String>,
+        transform: impl Fn(IN) -> OUT + Send + 'static,
+    ) -> Self {
         let event = Event::new(name);
         let event_handle = event.handle();
 
         let attachments = Arc::new(DashMap::new());
         let alive = Arc::new(());
-        let (event_queue_sender, event_queue_receiver) = unbounded_channel::<T>();
+        let (event_queue_sender, event_queue_receiver) = unbounded_channel::<IN>();
         spawn(async move {
-            run_forward_loop(event_handle, event_queue_receiver).await;
+            run_forward_loop(event_handle, event_queue_receiver, Box::new(transform)).await;
         });
 
         Self {
@@ -92,7 +101,7 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
         self.attachments.len()
     }
 
-    pub fn attach(&self, event_handle: impl Into<EventHandle<T>>) -> Result<(), AttachError> {
+    pub fn attach(&self, event_handle: impl Into<EventHandle<IN>>) -> Result<(), AttachError> {
         let event_handle = event_handle.into();
 
         event_handle.try_with(|event| {
@@ -112,7 +121,7 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
             let event_queue_sender = self.event_queue_sender.clone();
             let subscriber_id = event.subscribe_closure(
                 event_repeater_name.clone(),
-                move |data: T| -> Result<(), BoxedError> {
+                move |data: IN| -> Result<(), BoxedError> {
                     if alive.upgrade().is_none() {
                         return Err(Box::new(ForwardingError::RepeaterDropped {
                             event_repeater_name: event_repeater_name.clone(),
@@ -129,9 +138,14 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
                 true,
             );
 
+            let handle_for_is_dropped = event_handle.clone();
+            let handle_for_unsubscribe = event_handle.clone();
             let attachment = Attachment {
-                event: event_handle.clone(),
-                subscriber_id,
+                is_dropped: Box::new(move || handle_for_is_dropped.is_dropped()),
+                do_unsubscribe: Box::new(move || {
+                    let _ =
+                        handle_for_unsubscribe.try_with(|event| event.unsubscribe(subscriber_id));
+                }),
             };
 
             self.attachments.insert(event_id, attachment);
@@ -139,14 +153,73 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
         })?
     }
 
-    pub fn detach(&self, event_handle: impl Into<EventHandle<T>>) -> Result<(), DetachError> {
+    pub fn attach_with_transform<S: Clone + Send + 'static>(
+        &self,
+        event_handle: impl Into<EventHandle<S>>,
+        transform: impl Fn(S) -> IN + Send + Sync + 'static,
+    ) -> Result<(), AttachError> {
+        let event_handle = event_handle.into();
+
+        event_handle.try_with(|event| {
+            let event_id = event.id();
+            let event_repeater_name = self.name().to_string();
+
+            if self.attachments.contains_key(&event_id) {
+                let event_name = event.name().to_string();
+
+                return Err(AttachError::AlreadyAttached {
+                    event_repeater_name,
+                    event_name,
+                });
+            }
+
+            let alive = Arc::downgrade(&self.alive);
+            let event_queue_sender = self.event_queue_sender.clone();
+            let subscriber_id = event.subscribe_closure(
+                event_repeater_name.clone(),
+                move |data: S| -> Result<(), BoxedError> {
+                    if alive.upgrade().is_none() {
+                        return Err(Box::new(ForwardingError::RepeaterDropped {
+                            event_repeater_name: event_repeater_name.clone(),
+                        }));
+                    }
+
+                    let transformed = transform(data);
+                    event_queue_sender.send(transformed).map_err(|_| {
+                        Box::new(ForwardingError::RepeaterDropped {
+                            event_repeater_name: event_repeater_name.clone(),
+                        }) as BoxedError
+                    })
+                },
+                false,
+                true,
+            );
+
+            let handle_for_is_dropped = event_handle.clone();
+            let handle_for_unsubscribe = event_handle.clone();
+            let attachment = Attachment {
+                is_dropped: Box::new(move || handle_for_is_dropped.is_dropped()),
+                do_unsubscribe: Box::new(move || {
+                    let _ =
+                        handle_for_unsubscribe.try_with(|event| event.unsubscribe(subscriber_id));
+                }),
+            };
+
+            self.attachments.insert(event_id, attachment);
+            Ok(())
+        })?
+    }
+
+    pub fn detach<S: Clone + Send + 'static>(
+        &self,
+        event_handle: impl Into<EventHandle<S>>,
+    ) -> Result<(), DetachError> {
         let event_handle = event_handle.into();
 
         event_handle.try_with(|event| {
             let event_repeater_name = self.name().to_string();
-            let attachments = self.attachments.clone();
 
-            let attachment = match attachments.remove(&event.id()) {
+            let attachment = match self.attachments.remove(&event.id()) {
                 Some((_, attachment)) => attachment,
                 None => {
                     let event_name = event.name().to_string();
@@ -157,14 +230,11 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
                 }
             };
 
-            if attachment.event.is_dropped() {
+            if (attachment.is_dropped)() {
                 return Ok(());
             }
 
-            let _ = attachment
-                .event
-                .try_with(|event| event.unsubscribe(attachment.subscriber_id));
-
+            (attachment.do_unsubscribe)();
             Ok(())
         })?
     }
@@ -173,7 +243,7 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
         let dropped: Vec<u64> = self
             .attachments
             .iter()
-            .filter(|entry| entry.value().event.is_dropped())
+            .filter(|entry| (entry.value().is_dropped)())
             .map(|entry| *entry.key())
             .collect();
 
@@ -183,38 +253,44 @@ impl<T: Clone + Send + 'static> EventRepeater<T> {
     }
 }
 
-async fn run_forward_loop<T: Clone + Send + 'static>(
-    event_handle: EventHandle<T>,
-    mut event_queue_receiver: UnboundedReceiver<T>,
+async fn run_forward_loop<IN: Clone + Send + 'static, OUT: Clone + Send + 'static>(
+    event_handle: EventHandle<OUT>,
+    mut event_queue_receiver: UnboundedReceiver<IN>,
+    transform: Box<dyn Fn(IN) -> OUT + Send + 'static>,
 ) {
     while let Some(data) = event_queue_receiver.recv().await {
-        if event_handle.dispatch(data).await.is_err() {
+        let out = transform(data);
+        if event_handle.dispatch(out).await.is_err() {
             return;
         }
     }
 }
 
-impl<T: Clone + Send + 'static> PartialEq for EventRepeater<T> {
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> PartialEq for EventRepeater<IN, OUT> {
     fn eq(&self, other: &Self) -> bool {
         self.event.id() == other.event.id()
     }
 }
 
-impl<T: Clone + Send + 'static> PartialEq<u64> for EventRepeater<T> {
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> PartialEq<u64>
+    for EventRepeater<IN, OUT>
+{
     fn eq(&self, other: &u64) -> bool {
         self.event.id() == *other
     }
 }
 
-impl<T: Clone + Send + 'static> Eq for EventRepeater<T> {}
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> Eq for EventRepeater<IN, OUT> {}
 
-impl<T: Clone + Send + 'static> AsRef<Event<T>> for EventRepeater<T> {
-    fn as_ref(&self) -> &Event<T> {
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> AsRef<Event<OUT>>
+    for EventRepeater<IN, OUT>
+{
+    fn as_ref(&self) -> &Event<OUT> {
         &self.event
     }
 }
 
-impl<T: Clone + Send + 'static> Display for EventRepeater<T> {
+impl<IN: Clone + Send + 'static, OUT: Clone + Send + 'static> Display for EventRepeater<IN, OUT> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let sub_count = self.attachment_count();
         let sub_word = if sub_count == 1 {
@@ -360,7 +436,7 @@ mod tests {
         let event = Event::new("stop_test");
         let event_handle = event.handle();
         let task_handle = spawn(async move {
-            run_forward_loop(event_handle, event_queue_receiver).await;
+            run_forward_loop(event_handle, event_queue_receiver, Box::new(|x| x)).await;
         });
 
         drop(event_queue_sender); // Closes the channel, which will cause the forward loop to exit
@@ -385,5 +461,146 @@ mod tests {
         event1.dispatch(DATA).await.unwrap();
         let received_data = receiver.recv().await.unwrap();
         assert_eq!(received_data, DATA);
+    }
+
+    #[tokio::test]
+    async fn repeat_data_with_attach_transform() {
+        let event_repeater: EventRepeater<usize> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<String> = Event::new(EVENT_NAME);
+        let event1_handle = event1.handle();
+
+        event_repeater
+            .attach_with_transform(event1_handle, |s| s.len())
+            .unwrap();
+
+        let mut receiver = event_repeater
+            .event
+            .subscribe_channel(RECEIVER_NAME, 1, false, true)
+            .1;
+
+        event1.dispatch("hello".to_string()).await.unwrap();
+        let received_data = receiver.recv().await.unwrap();
+        assert_eq!(received_data, 5);
+    }
+
+    #[tokio::test]
+    async fn repeat_data_with_repeater_transform() {
+        let event_repeater: EventRepeater<u16, String> =
+            EventRepeater::new_with_transform(REPEATER_NAME, |n: u16| n.to_string());
+        let event1: Event<u16> = Event::new(EVENT_NAME);
+        let event1_handle = event1.handle();
+
+        event_repeater.attach(event1_handle).unwrap();
+
+        let mut receiver = event_repeater
+            .event
+            .subscribe_channel(RECEIVER_NAME, 1, false, true)
+            .1;
+
+        event1.dispatch(DATA).await.unwrap();
+        let received_data = receiver.recv().await.unwrap();
+        assert_eq!(received_data, DATA.to_string());
+    }
+
+    #[tokio::test]
+    async fn repeat_data_with_both_transforms() {
+        let event_repeater: EventRepeater<usize, String> =
+            EventRepeater::new_with_transform(REPEATER_NAME, |n: usize| n.to_string());
+        let event1: Event<String> = Event::new(EVENT_NAME);
+
+        event_repeater
+            .attach_with_transform(event1.handle(), |s| s.len())
+            .unwrap();
+
+        let mut receiver = event_repeater
+            .event
+            .subscribe_channel(RECEIVER_NAME, 1, false, true)
+            .1;
+
+        event1.dispatch("hello".to_string()).await.unwrap();
+        let received_data = receiver.recv().await.unwrap();
+        assert_eq!(received_data, "5");
+    }
+
+    #[tokio::test]
+    async fn attach_with_transform_lifecycle() {
+        let event_repeater: EventRepeater<usize> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<String> = Event::new(EVENT_NAME);
+        let event1_handle = event1.handle();
+
+        assert_eq!(event_repeater.attachment_count(), 0);
+        assert_eq!(event1.subscriber_count(), 0);
+
+        event_repeater
+            .attach_with_transform(event1_handle.clone(), |s| s.len())
+            .unwrap();
+
+        assert_eq!(event_repeater.attachment_count(), 1);
+        assert_eq!(event1.subscriber_count(), 1);
+
+        event_repeater.detach(event1_handle).unwrap();
+
+        assert_eq!(event_repeater.attachment_count(), 0);
+        assert_eq!(event1.subscriber_count(), 0);
+    }
+
+    #[tokio::test]
+    async fn remove_closed_attachments_with_transform() {
+        let event_repeater: EventRepeater<usize> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<String> = Event::new(EVENT_NAME);
+
+        event_repeater
+            .attach_with_transform(event1.handle(), |s| s.len())
+            .unwrap();
+
+        drop(event1);
+
+        assert_eq!(event_repeater.attachment_count(), 0);
+    }
+
+    #[tokio::test]
+    async fn remove_from_events_on_drop_with_transform() {
+        let event_repeater: EventRepeater<usize> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<String> = Event::new(EVENT_NAME);
+
+        event_repeater
+            .attach_with_transform(event1.handle(), |s: String| s.len())
+            .unwrap();
+        assert_eq!(event1.subscriber_count(), 1);
+
+        drop(event_repeater);
+        assert_eq!(event1.subscriber_count(), 1); // forwarding closure not yet removed
+
+        let result = event1.dispatch("hello".to_string()).await; // triggers forwarding closure, which errors and self-removes
+        assert!(result.is_err());
+        assert_eq!(event1.subscriber_count(), 0);
+    }
+
+    #[tokio::test]
+    async fn attach_with_transform_already_attached() {
+        let event_repeater: EventRepeater<usize> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<String> = Event::new(EVENT_NAME);
+        let event1_handle = event1.handle();
+
+        event_repeater
+            .attach_with_transform(event1_handle.clone(), |s| s.len())
+            .unwrap();
+
+        let result = event_repeater.attach_with_transform(event1_handle, |s| s.len() + 1);
+
+        assert!(matches!(result, Err(AttachError::AlreadyAttached { .. })));
+    }
+
+    #[tokio::test]
+    async fn attach_and_attach_with_transform_already_attached() {
+        let event_repeater: EventRepeater<()> = EventRepeater::new(REPEATER_NAME);
+        let event1: Event<()> = Event::new(EVENT_NAME);
+        let event1_handle = event1.handle();
+
+        event_repeater.attach(event1_handle.clone()).unwrap();
+
+        let result = event_repeater.attach_with_transform(event1_handle, |x: ()| x);
+
+        assert!(matches!(result, Err(AttachError::AlreadyAttached { .. })));
     }
 }
